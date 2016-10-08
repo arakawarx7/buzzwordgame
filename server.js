7054afe3d04d04b683727b50f95ef9776d9ec0b4
Aaron <aaron.devleague@gmail.com>
2016-10-07 18:39:43 -1000

    got my get and post to sorta work

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = 3000;
 
-var words=[];
+var wordArray = [];
+function createBuzzWord(word,points){
+  var bWords=[{ "buzzWord": word, "points": points }];
+  wordArray.push(bWords);
+  return { "success": true };
+}
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
 
 app.get('/buzzwords',(req,res)=>{
-  res.json({'buzzwords':[]});
+  res.json(wordArray);
 });
 
+app.post('/buzzwords',(req,res)=>{
 
+ let holder = createBuzzWord(req.body.buzzWord,req.body.points);
+ console.log('createbuzzword',holder);
+ res.json(holder);
+});
 
 const server = app.listen(PORT,()=>{
   console.log(`Server started on ${PORT}`);
