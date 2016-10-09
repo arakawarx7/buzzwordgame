eb24c09d7b56e7ea5e2c36ec7cccffdfa1306f28
Aaron <aaron.devleague@gmail.com>
2016-10-08 18:56:16 -1000

    finished the delete

--- routes/data.js
+++ routes/data.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const router = express.Router();
+// const express = require('express');
+// const router = express.Router();
 
-router.route('/')
-  .get((req,res)=>{
-    res.send('get a word');
-  })
-
-
+// router.route('/')
+//   .get((req,res)=>{
+//     res.send('get a word');
+//   })
 
 
- module.exports = router;
+
+
+//  module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const bodyParser = require('body-parser');
 const PORT = 3000;
 
 var wordArray = [];
+var updatedScore = 0;
 function createBuzzWord(word,points){
-  var bWords={ "buzzWord": word, "points": parseInt(points) };
+  var bWords={
+    "buzzWord": word,
+    "points": parseInt(points)
+  };
   wordArray.push(bWords);
   return { success: true };
 }
@@ -16,25 +20,88 @@ app.use(express.static('public'));
 
 app.get('/buzzwords',(req,res)=>{
   res.json(wordArray);
+  console.log("get");
 });
 
 app.post('/buzzwords',(req,res)=>{
-  var duplicateFound = false;
-  //search the array for any duplicate buzzwords
-  for(var i = 0; i < wordArray.length; i++){
-    if(wordArray[i].buzzWord === req.body.buzzWord){
-     //if duplicate is found take note of it
-      duplicateFound = true;
-    }
-  }
-  //if no duplicate is found, add new buzzword to the array.
+  var duplicateFound = wordArray.find((word)=>{
+    //if True and buzz word is found then do nothing
+    console.log("duplicate found");
+    return word.buzzWord === req.body.buzzWord;
+  });
   if(!duplicateFound){
+    console.log("duplicate not found");
     createBuzzWord(req.body.buzzWord,req.body.points);
   }
-   return res.json({
-    //if no duplicate is found return success true.
+  return res.json({
+    //if no duplicate is found return success false.
     sucess: !duplicateFound
-   });
+  });
+});
+
+app.put('/buzzwords',(req,res)=>{
+  //if True and buzz word is found then do nothing
+  var duplicateFound = wordArray.find((wordObj)=>{
+    return wordObj.buzzWord === req.body.buzzWord;
+  });
+  var responseObj = {};
+
+  if(duplicateFound){
+    responseObj.sucess = true;
+    responseObj.newScore =  updatedScore += parseInt(duplicateFound.points);
+    console.log('updated score',updatedScore);
+  } else {
+    console.log(`there is no word`);
+    responseObj.sucess = false;
+  }
+
+  return res.json(responseObj);
+});
+
+
+
+app.delete('/buzzwords',(req,res)=>{
+  //if True and buzz word is found then do nothing
+   wordArray = wordArray.filter((wordObj)=>{
+    console.log("delete word");
+    console.log( "wordObj.buzzWord",wordObj.buzzWord) ;
+    console.log('req.body.buzzword',req.body.buzzWord);
+    return wordObj.buzzWord !== req.body.buzzWord;
+  });
+   console.log("wordArray",wordArray);
+  // var responseObj = {};
+
+  // if(wordArray){
+  //   responseObj.sucess = true;
+
+  // } else {
+  //   console.log(`there is no word`);
+  //   responseObj.sucess = false;
+  // }
+
+   res.json({'sucess': true});
+});
+
+app.post('/reset',(req,res)=>{
+  //if True and buzz word is found then do nothing
+   wordArray = wordArray.filter((wordObj)=>{
+    console.log("delete word");
+    console.log( "wordObj.buzzWord",wordObj.buzzWord) ;
+    console.log('req.body.buzzword',req.body.buzzWord);
+    delete wordObj.buzzWord !== req.body.buzzWord;
+  });
+   console.log("wordArray",wordArray);
+  // var responseObj = {};
+
+  // if(wordArray){
+  //   responseObj.sucess = true;
+
+  // } else {
+  //   console.log(`there is no word`);
+  //   responseObj.sucess = false;
+  // }
+
+   res.json({'sucess': true});
 });
 
 const server = app.listen(PORT,()=>{
